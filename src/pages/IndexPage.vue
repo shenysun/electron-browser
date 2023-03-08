e0aecfdf789c1e970cea0bc118b0c8b042b24ac7
sunyongshen <sunyongshen@tal.com>
2023-03-08 14:38:38 +0800

    feat: 项目初始化

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="click">frame</button>
+    <iframe ref="frame" src="https://www.bing.com" frameborder="0"></iframe>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const frame = ref<HTMLIFrameElement>();
+onMounted(() => {
+    if (!frame.value) {
+        return;
+    }
+
+    frame.value.onload = (v) => {
+        console.log('hashchange', v);
+    };
+});
+function click() {
+    console.log(frame.value);
+    console.log(frame.value?.contentWindow?.location.href);
+}
+</script>
